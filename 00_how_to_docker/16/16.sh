8ba7957218503de550aaddffe68fbe73b2c5f356
Thulasizwe MAVUSO <thumavu@e4r7p6.wethinkcode.co.za>
2017-11-28 16:53:15 +0200

    okay

---
+++ 00_how_to_docker/16/16.sh
@@ -0,0 +1,4 @@
+# logs == batch retrieves command present at the time of execution
+# -f or --follow == follow log output
+# The docker logs --follow command will continue streaming the new output from the container’s STDOUT and STDERR.
+docker logs spawning-pool --follow
---
+++ 00_how_to_docker/17/17.sh
@@ -0,0 +1,2 @@
+# docker ps -a == to list all containers in a vm
+docker ps
